190283bc65288e1e82bc5436f24874624ebe7a8a
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 23:03:10 -0500

    Update DronEmergencia.java

--- src/DronEmergencia.java
+++ src/DronEmergencia.java
@@ -2,7 +2,7 @@ public class DronEmergencia extends Dron{
     private double recargoUrgencia;
     private int nivelPrioridad;
     public DronEmergencia(){
-        super();
+        super("EMG001", "Emergente", 8.0, 3.0, 1.0, 15.0);
     }
 
     public double getRecargoUrgencia() {
@@ -23,6 +23,26 @@ public class DronEmergencia extends Dron{
 
     @Override
     public double calcularCostoEntrega(){
+        return getCostoBase() +(getDistanciakm() * 1.00) + recargoUrgencia;
+    }
+
+    @Override
+    public boolean validarDatos(){
+        if(getPesoPaquete() <= 10 && getHorasVuelo() <= 3 && nivelPrioridad >= 1 && nivelPrioridad <= 3){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void mostrarInformacion(){
+        System.out.println("--- Dron Emergencia ---");
+        super.mostrarInformacion();
+        System.out.println("Nivel de prioridad: " + nivelPrioridad);
+        System.out.println("Costo de entrega: " + calcularCostoEntrega());
+    }
+}
         return costoBase +(getDistanciakm() * 1.00) + recargoUrgencia;
     }
 
