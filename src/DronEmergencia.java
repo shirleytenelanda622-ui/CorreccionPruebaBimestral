ff935cc1f45ea0a9a95950d3532b1f9f8fbe51a4
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 23:04:02 -0500

    Update DronEmergencia.java

--- src/DronEmergencia.java
+++ src/DronEmergencia.java
@@ -43,20 +43,3 @@ public class DronEmergencia extends Dron{
         System.out.println("Costo de entrega: " + calcularCostoEntrega());
     }
 }
-        return costoBase +(getDistanciakm() * 1.00) + recargoUrgencia;
-    }
-
-    @Override
-    public boolean validarDatos(){
-        if(getPesoPaquete() <= 10 && getHorasVuelo() <= 3 && nivelPrioridad >= 1 && nivelPrioridad <= 3){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public void mostrarInformacion(){
-        System.out.println("Costo de entrega: " + calcularCostoEntrega());
-    }
-}
